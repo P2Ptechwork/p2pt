b59413e1d3c58e3cd0afb179da98841aecb2ebd3
P2Ptechwork <159875250+P2Ptechwork@users.noreply.github.com>
2024-02-17 20:43:17 +0530

    "file renaming"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/main_page/main_page';
-import RegistrationPage from './components/Sch_registration/Sch_registration';
-import St_register from './components/St_registration/St_registration'
+import RegistrationPage from './components/SchRegistration/SchRegistration';
+import StRegister from './components/StRegistration/StRegistration'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="/st_register" element={<St_register />} />
+        <Route path="/stregister" element={<StRegister />} />
         <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
